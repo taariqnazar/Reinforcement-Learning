ea210c76327cc1214275ea3024303fe6afa8a7c8
Tariq <tariq.nazar13@gmail.com>
2021-12-15 22:11:48 +0100

    merged labs

---
+++ lab2/agent.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+class DQN_Agent:
+
+    def __init__(self):
+        pass
+
+    def __init_network(self):
+        pass
+
+    def store_buffer(self):
+        pass
+
+    def update_network(self):
+        pass
+    
